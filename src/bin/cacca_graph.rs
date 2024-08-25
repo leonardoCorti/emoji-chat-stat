2effa46404449c3e98a92b78b911ac3008ebc761
Leonardo Corti <leonardo.filippo@ymail.com>
2024-08-25 18:40:59 +0200

    added graph for days

--- src/bin/cacca_graph.rs
+++ src/bin/cacca_graph.rs
@@ -1,9 +1,11 @@
+use chrono::{NaiveDate, Weekday};
 use csv::Reader;
 use plotters::prelude::*;
 use std::collections::HashMap;
 use std::error::Error;
 use image::{DynamicImage, GenericImage, RgbaImage};
 use std::env;
+use chrono::Datelike;
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Retrieve the first command-line argument as the file path
@@ -15,6 +17,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let file_path = &args[1];
     
     // Reading and parsing the CSV file
+    // csv file is formatted like:dd/mm/yy,hh:mm,name 
     let mut reader = Reader::from_path(file_path)?;
 
     let mut data: HashMap<String, [u32; 24]> = HashMap::new();
@@ -29,17 +32,36 @@ fn main() -> Result<(), Box<dyn Error>> {
             .or_insert([0; 24])[hour] += 1;
     }
 
+    let mut reader = Reader::from_path(file_path)?;
+    let mut data_day: HashMap<String, HashMap<Weekday, u32>> = HashMap::new();
+    for result in reader.records() {
+        let record = result?;
+        let date_str = &record[0];
+        let date = NaiveDate::parse_from_str(date_str, "%d/%m/%y")?;
+        let name = record[2].to_string();
+        let weekday = date.weekday();
+        *data_day
+            .entry(name.clone()).or_insert(HashMap::new())
+            .entry(weekday).or_insert(0) += 1;
+    }
+
+    let mut filenames_by_weekday = Vec::new();
+    for (name, day_count) in &data_day {
+        create_histogram_weekday(name, day_count)?;
+        filenames_by_weekday.push(format!("{}-by-weekday.png", name));   
+    }
+
     let mut filenames_by_hour = Vec::new();
     // Creating histograms for each name
     for (name, hour_counts) in &data {
-        create_histogram(name, hour_counts)?;
+        create_histogram_hours(name, hour_counts)?;
         filenames_by_hour.push(format!("{}-by-hour.png", name));
     }
 
     let one_image = args.contains(&"--one-image".to_string());
     if one_image {
-        // Merge all generated images into a single image
         merge_images(&filenames_by_hour, "all-by-hour.png")?;
+        merge_images(&filenames_by_weekday, "all-by-weekday.png")?;
     }
 
     Ok(())
@@ -72,7 +94,65 @@ fn merge_images(image_paths: &[String], output_filename: &str) ->  Result<(), Bo
     Ok(())
 }
 
-fn create_histogram(name: &str, hour_counts: &[u32; 24]) -> Result<(), Box<dyn Error>> {
+fn create_histogram_weekday(name: &str, day_count: &HashMap<Weekday, u32>) -> Result<(), Box<dyn Error>> {
+    let filename = format!("{}-by-weekday.png", name);
+    let root = BitMapBackend::new(&filename, (640, 480)).into_drawing_area();
+    root.fill(&WHITE)?;
+
+    let weekdays = vec![
+        Weekday::Mon,
+        Weekday::Tue,
+        Weekday::Wed,
+        Weekday::Thu,
+        Weekday::Fri,
+        Weekday::Sat,
+        Weekday::Sun,
+    ];
+
+    let counts: Vec<u32> = weekdays.iter().map(|&d| day_count.get(&d).cloned().unwrap_or(0)).collect();
+    let max_count = *counts.iter().max().unwrap_or(&0);
+
+    let mut chart = ChartBuilder::on(&root)
+        .caption(format!("distribuzione settimanale {}", name), ("sans-serif", 40))
+        .x_label_area_size(35)
+        .y_label_area_size(40)
+        .margin(5)
+        .build_cartesian_2d(0..7, 0u32..max_count)?;
+
+    chart.configure_mesh()
+        .x_desc("giorno della settimana")
+        .y_desc("conta")
+        .x_labels(7)
+        .x_label_formatter(&|x| match x {
+            0 => "Mon".to_string(),
+            1 => "Tue".to_string(),
+            2 => "Wed".to_string(),
+            3 => "Thu".to_string(),
+            4 => "Fri".to_string(),
+            5 => "Sat".to_string(),
+            6 => "Sun".to_string(),
+            _ => "".to_string(),
+        })
+        .draw()?;
+
+    chart.draw_series(
+        Histogram::vertical(&chart)
+            .style(BLUE.mix(0.5).filled())
+            .data(
+                weekdays.iter().enumerate()
+                    .map(|(i, &d)| (i as i32, day_count.get(&d).cloned()
+                        .unwrap_or(0))
+                    )
+            ),
+    )?;
+
+    root.present()?;
+    println!("Saved histogram to {}", filename);
+
+    Ok(())
+}
+
+fn create_histogram_hours(name: &str, hour_counts: &[u32; 24]) -> Result<(), Box<dyn Error>> {
     let filename = format!("{}-by-hour.png", name);
     let root = BitMapBackend::new(&filename, (640, 480)).into_drawing_area();
     root.fill(&WHITE)?;
