ed0a6fb6940dc7d697ceb53751ef0a623057145c
Leonardo Corti <leonardo.filippo@ymail.com>
2024-08-26 11:42:48 +0200

    changed name for executables

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,31 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() != 2 {
-        eprintln!("Usage: bin0 <input.txt>");
+        eprintln!("Usage: emoji-chat-stat  <input.txt>");
         exit(1);
     }
 
     let input_file = &args[1];
     let output_file = input_file.replace(".txt", ".csv");
 
-    let status = Command::new("cacca_text.exe")
+    let status = Command::new("emoji2csv")
         .arg(input_file)
         .status()
-        .expect("Failed to execute cacca_text");
+        .expect("Failed to execute emoji2csv");
 
     if !status.success() {
-        eprintln!("cacca_text failed");
+        eprintln!("emoji2csv failed");
         exit(1);
     }
 
-    let status = Command::new("cacca_graph.exe")
+    let status = Command::new("emojicsv2graph")
         .arg(output_file)
         .arg("--one-image")
         .status()
-        .expect("Failed to execute cacca_graph");
+        .expect("Failed to execute emojicsv2graph");
 
     if !status.success() {
-        eprintln!("cacca_graph failed");
+        eprintln!("emojicsv2graph failed");
         exit(1);
     }
 }
